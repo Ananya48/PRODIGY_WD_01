5de7c8e23e79065f3d59b07503fa660067c692d9
Ananya48 <131737127+Ananya48@users.noreply.github.com>
2024-03-24 12:50:43 +0530

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', function() {
     const bar = document.getElementById('bar');
-    if (window.scrollY > 50) 
+    if (window.scrollY > 50){
         bar.classList.add('scrolled');
     } else {     
         bar.classList.remove('scrolled');
